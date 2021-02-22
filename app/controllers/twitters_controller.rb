d7a276db932473c7cc83a57e8bba07b04f0beb41
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2021-02-22 23:58:26 +0900

    feat:APIを叩いてトレンド情報を取得する機能を実装

--- app/controllers/twitters_controller.rb
+++ app/controllers/twitters_controller.rb
@@ -1,5 +1,12 @@
 class TwittersController < ApplicationController
   def trend
-    
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_API_KEY"]
+      config.consumer_secret     = ENV["TWITTER_API_SECRET_KEY"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET_TOKEN"]
+    end
+    trends = client.trends
+    trends.first.attrs
   end
 end
